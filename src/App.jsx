6dda163bf3e55cdf2cc070a1fd4d30a5efc46864
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-11-29 00:03:20 +0600

    Routes setup and Layout set

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className="">
 
-        <h1 className='text-xl font-poppins text-center mt-20'>Art Shop Booking Store Explore</h1>
+        
 
         <p className=''></p>
 
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-xl font-poppins text-center mt-20'>Art Shop Booking Store Explore - Home</h1>
+        </div>
+    );
+};
+
+export default Home;
