63ee36c1b708985349230149c0e9df60188271a0
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-11-28 23:52:32 +0600

    projects setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="">
+
+        <h1 className='text-xl font-poppins text-center mt-20'>Art Shop Booking Store Explore</h1>
+
+        <p className=''></p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
